a183b1379384e1daf1bee509f54c4343a3830a00
Vladislav Nazukin <vlad.nazukin@mail.ru>
2022-06-10 16:40:16 +0400

    Убрал лишние принты, теперь не пишет в воскресенье

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ logging.basicConfig(filename='logs.log', filemode='w',
 settings = json.load(open("settings.json", 'rb'))
 # Подгрузка настроек
 
+u1MINUTE = 60 # 1 минута
 u1HOUR = 3600  # 1 час
 u1DAY = 86400  # день
 notFindChangesSleepDelay = u1HOUR * 4
@@ -35,6 +36,8 @@ async def main():
         today = datetime.date.today() + datetime.timedelta(days=1)
         if lastNotification == today:
             raise ChildProcessError
+        elif today.isoweekday() == 7:
+            raise BrokenPipeError
         monthformat = today.month if len(str(today.month)) == 2 else "0" + str(today.month)
         dayformat = today.day if len(str(today.day)) == 2 else "0" + str(today.day)
         changes = requests.get(f"http://tpcol.ru/images/Расписание_и_Замены/{dayformat}.{monthformat}.{today.year}.xlsx")
@@ -79,7 +82,7 @@ async def main():
                 lastNotification = today
         else:
             logging.info(
-                f"Замены на {today.day}.{monthformat}.{today.year} ещё не выложили. Проверю ещё раз через 1 час")
+                f"Замены на {today.day}.{monthformat}.{today.year} ещё не выложили. Проверю ещё раз через 10 минут")
             # await api.api.messages.send(peer_id=myVK_ID,
             #                             random_id=random.randrange(999999),
             #                             message=f"Замены на {today.day}.{monthformat}.{today.year} "
@@ -89,15 +92,17 @@ async def main():
             os.remove(f"{today.day}.{monthformat}.{today.year}.xlsx")
         except Exception as e:
             logging.warning(f"Ошибка при удалении файла {e}")
-        await asyncio.sleep(u1HOUR * 1)
+        await asyncio.sleep(u1MINUTE * 10)
         # await asyncio.sleep(10)
     except NotImplementedError:
         # Когда при загрузке файла его не было
-        await asyncio.sleep(u1HOUR * 1)
-        logging.error("Проверяю ещё раз наличие замен спустя 1 час.")
+        logging.error("Проверяю ещё раз наличие замен спустя 10 минут.")
     except ChildProcessError:
         logging.error("Сегодня уже было уведомление о заменах, скипаю на 1 час.")
         await asyncio.sleep(u1HOUR * 1)
+    except BrokenPipeError:
+        logging.error("Сегодня суббота, через 4 часа ещё раз посмотрю.")
+        await asyncio.sleep(u1HOUR * 4)
     except Exception as e:
         logging.error(f"Произошла ошибка {e}.")
 
@@ -105,8 +110,6 @@ async def main():
 if __name__ == "__main__":
     logging.info(f"Токен подгружен. Бот работает.")
     while True:
-        logging.info(f"Начинаем проверять замены по циклу")
-
         loop = asyncio.new_event_loop()
         tasks = [
             loop.create_task(main())
