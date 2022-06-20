37f4340823042fe9dc18daf2bff8653430011728
Vladislav Nazukin <32748041+Vladislav875@users.noreply.github.com>
2022-06-20 09:50:30 +0300

    Добавил проверку на отсутствие пары и сортировку

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ async def main():
         #     raise BrokenPipeError
         monthformat = today.month if len(str(today.month)) == 2 else "0" + str(today.month)
         dayformat = today.day if len(str(today.day)) == 2 else "0" + str(today.day)
+        changeslist = []
         changes = requests.get(f"http://tpcol.ru/images/Расписание_и_Замены/{dayformat}.{monthformat}.{today.year}.xlsx")
         # http://tpcol.ru/images/Расписание_и_Замены/26.05.2022.xlsx
         if changes.status_code == 200 and "<!DOCTYPE html>" not in str(changes.content):  # Файл есть
@@ -61,13 +62,7 @@ async def main():
                         bchanges = True
                         logging.info(
                             f"Найдена замена на {today.day}.{monthformat}.{today.year} для группы \"{group}\".")
-                        await api.api.messages.send(peer_id=myVK_ID,
-                                                    random_id=random.randrange(999999),
-                                                    message=f"""
-🔔 Замена на {i[3]} паре
-🤓 Замена на: {i[6]}
-👥 Преподаватель: {i[5]}
-🏚 Кабинет: {i[7]} каб.""")
+                        changeslist.append([int(i[3]), i[6], i[5], i[7]])
                 except Exception as e:
                     await api.api.messages.send(peer_id=myVK_ID,
                                                 random_id=random.randrange(999999),
@@ -80,20 +75,35 @@ async def main():
                                             message=f"Замен для группы \"{group}\" нету.")
                 logging.info(f"Замен на {today.day}.{monthformat}.{today.year} для группы \"{group}\" нету.")
                 lastNotification = today
+            changeslist.sort()
+            for lschange in changeslist:
+                if lschange[1].lower() == "нет":
+                    await api.api.messages.send(peer_id=myVK_ID,
+                                                random_id=random.randrange(999999),
+                                                message=f"💦 {lschange[0]} пары не будет")
+                else:
+                    await api.api.messages.send(peer_id=myVK_ID,
+                                                random_id=random.randrange(999999),
+                                                message=f"""
+    🔔 Замена на {lschange[0]} паре
+    🤓 Замена на: {lschange[1]}
+    👥 Преподаватель: {lschange[2]}
+    🏚 Кабинет: {lschange[3]} каб.""")
+                
         else:
             logging.info(
-                f"Замены на {today.day}.{monthformat}.{today.year} ещё не выложили. Проверю ещё раз через 10 минут")
+                f"Замены на {today.day}.{monthformat}.{today.year} ещё не выложили. Проверю ещё раз через 1 минуту")
             # await api.api.messages.send(peer_id=myVK_ID,
             #                             random_id=random.randrange(999999),
             #                             message=f"Замены на {today.day}.{monthformat}.{today.year} "
             #                                     f"ещё не выложили или их нет. Перепроверю ещё раз через 4 часа")
-            await asyncio.sleep(u1MINUTE * 10)
+            await asyncio.sleep(u1MINUTE)
             raise NotImplementedError
         try:
             os.remove(f"{today.day}.{monthformat}.{today.year}.xlsx")
         except Exception as e:
             logging.warning(f"Ошибка при удалении файла {e}")
-        await asyncio.sleep(u1MINUTE * 10)
+        await asyncio.sleep(u1MINUTE)
         # await asyncio.sleep(10)
     except NotImplementedError:
         # Когда при загрузке файла его не было
@@ -104,8 +114,8 @@ async def main():
     except BrokenPipeError:
         logging.error("Сегодня суббота, через 4 часа ещё раз посмотрю.")
         await asyncio.sleep(u1HOUR * 4)
-    except Exception as e:
-        logging.error(f"Произошла ошибка {e}.")
+    # except Exception as e:
+    #     logging.error(f"Произошла ошибка {e}.")
 
 
 if __name__ == "__main__":
